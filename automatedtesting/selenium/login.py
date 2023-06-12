697c435be6c90a136ac5526170ab68cf23b1f87e
Joyon <Ahmed.Joyon@riverty.com>
2023-06-12 14:39:10 +0300

    de

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -7,11 +7,13 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+    options.add_argument("--no-sandbox")
+    options.add_argument('--ignore-certificate-errors')
     print ('Browser started successfully. Navigating to the demo page to login.')
+    driver = webdriver.Chrome(options=options, executable_path='/home/adminuser/app/chromedriver')
     driver.get('https://www.saucedemo.com/')
 
 login('standard_user', 'secret_sauce')
